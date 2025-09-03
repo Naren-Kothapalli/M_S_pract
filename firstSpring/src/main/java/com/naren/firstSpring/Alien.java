3b4d2dd99c6d272da9dadde095706889ffcbcbc0
Kothapalli <2438062@cognizant.com>
2025-09-03 18:03:20 +0530

    added spring

---
+++ firstSpring/src/main/java/com/naren/firstSpring/Alien.java
@@ -0,0 +1,8 @@
+package com.naren.firstSpring;
+
+public class Alien {
+	public void code() {
+		System.out.println("coding");
+	}
+
+}
--- firstSpring/src/main/java/com/naren/firstSpring/Application.java
+++ firstSpring/src/main/java/com/naren/firstSpring/Application.java
@@ -1,16 +1,40 @@
 package com.naren.firstSpring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
+ * Main application class for Java-based configuration.
  *
- * Spring Boot application starter class
+ * The @Configuration annotation tells Spring that this class contains bean definitions.
+ * It's the Java equivalent of an XML bean definition file.
  */
-@SpringBootApplication
+@Configuration
 public class Application {
+
+    /**
+     * This method creates and returns a MyService object.
+     *
+     * The @Bean annotation tells Spring to register the object returned by this method
+     * as a bean in the application context. By default, the bean's name is the
+     * method name ("myService").
+     * @return a new instance of MyService.
+     */
+    @Bean
+    public Alien myService() {
+        return new Alien();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        System.out.println("Hello world naren");
+        // Create the application context by loading the AppConfig class.
+        // This is where Spring reads the configuration and creates the beans.
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+
+        // Retrieve the bean from the context.
+        Alien service = context.getBean(Alien.class);
+        service.code();
+
+       
     }
 }
