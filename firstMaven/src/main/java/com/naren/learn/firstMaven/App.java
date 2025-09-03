48044f9f48c2558c5aceef8d432f0661a8610d78
Kothapalli <2438062@cognizant.com>
2025-09-03 11:23:48 +0530

    Added firstMaven project files

---
+++ firstMaven/src/main/java/com/naren/learn/firstMaven/App.java
@@ -0,0 +1,10 @@
+package com.naren.learn.firstMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
