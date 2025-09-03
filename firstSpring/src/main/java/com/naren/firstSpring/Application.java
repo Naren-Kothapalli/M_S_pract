926a78e3a2f7c87bbf9a332c763a71c2b1dfb5b6
Kothapalli <2438062@cognizant.com>
2025-09-03 11:47:59 +0530

    updated poms

---
+++ firstSpring/src/main/java/com/naren/firstSpring/Application.java
@@ -0,0 +1,16 @@
+package com.naren.firstSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Hello world naren");
+    }
+}
